1b4280a92d3f421b26b9901c1fc9e27d9561a4a8
ataberk10 <ataberk.akcin@edu.rtu.lv>
2025-03-31 23:11:27 +0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace GameVsComputer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.TextBox txtStartNumber;
+        private System.Windows.Forms.ComboBox cmbAlgorithm;
+        private System.Windows.Forms.ComboBox cmbStartPlayer;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnMult2;
+        private System.Windows.Forms.Button btnMult3;
+        private System.Windows.Forms.Button btnMult4;
+        private System.Windows.Forms.Label lblCurrentNumber;
+        private System.Windows.Forms.Label lblScores;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtStartNumber = new System.Windows.Forms.TextBox();
+            this.cmbAlgorithm = new System.Windows.Forms.ComboBox();
+            this.cmbStartPlayer = new System.Windows.Forms.ComboBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnMult2 = new System.Windows.Forms.Button();
+            this.btnMult3 = new System.Windows.Forms.Button();
+            this.btnMult4 = new System.Windows.Forms.Button();
+            this.lblCurrentNumber = new System.Windows.Forms.Label();
+            this.lblScores = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtStartNumber
+            // 
+            this.txtStartNumber.Location = new System.Drawing.Point(20, 20);
+            this.txtStartNumber.Name = "txtStartNumber";
+            this.txtStartNumber.Size = new System.Drawing.Size(100, 22);
+            this.txtStartNumber.TabIndex = 0;
+            // 
+            // cmbAlgorithm
+            // 
+            this.cmbAlgorithm.Location = new System.Drawing.Point(140, 20);
+            this.cmbAlgorithm.Name = "cmbAlgorithm";
+            this.cmbAlgorithm.Size = new System.Drawing.Size(121, 24);
+            this.cmbAlgorithm.TabIndex = 1;
+            // 
+            // cmbStartPlayer
+            // 
+            this.cmbStartPlayer.Location = new System.Drawing.Point(280, 20);
+            this.cmbStartPlayer.Name = "cmbStartPlayer";
+            this.cmbStartPlayer.Size = new System.Drawing.Size(121, 24);
+            this.cmbStartPlayer.TabIndex = 2;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(420, 20);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(100, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start Game";
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnMult2
+            // 
+            this.btnMult2.Location = new System.Drawing.Point(20, 60);
+            this.btnMult2.Name = "btnMult2";
+            this.btnMult2.Size = new System.Drawing.Size(75, 30);
+            this.btnMult2.TabIndex = 4;
+            this.btnMult2.Text = "x2";
+            this.btnMult2.Click += new System.EventHandler(this.btnMult2_Click);
+            // 
+            // btnMult3
+            // 
+            this.btnMult3.Location = new System.Drawing.Point(110, 60);
+            this.btnMult3.Name = "btnMult3";
+            this.btnMult3.Size = new System.Drawing.Size(75, 30);
+            this.btnMult3.TabIndex = 5;
+            this.btnMult3.Text = "x3";
+            this.btnMult3.Click += new System.EventHandler(this.btnMult3_Click);
+            // 
+            // btnMult4
+            // 
+            this.btnMult4.Location = new System.Drawing.Point(200, 60);
+            this.btnMult4.Name = "btnMult4";
+            this.btnMult4.Size = new System.Drawing.Size(75, 30);
+            this.btnMult4.TabIndex = 6;
+            this.btnMult4.Text = "x4";
+            this.btnMult4.Click += new System.EventHandler(this.btnMult4_Click);
+            // 
+            // lblCurrentNumber
+            // 
+            this.lblCurrentNumber.Location = new System.Drawing.Point(20, 110);
+            this.lblCurrentNumber.Name = "lblCurrentNumber";
+            this.lblCurrentNumber.Size = new System.Drawing.Size(300, 20);
+            this.lblCurrentNumber.TabIndex = 7;
+            this.lblCurrentNumber.Text = "Current Number: -";
+            // 
+            // lblScores
+            // 
+            this.lblScores.Location = new System.Drawing.Point(20, 140);
+            this.lblScores.Name = "lblScores";
+            this.lblScores.Size = new System.Drawing.Size(400, 20);
+            this.lblScores.TabIndex = 8;
+            this.lblScores.Text = "Scores: -";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(197, 192);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(400, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Ataberk AKCIN";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 211);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtStartNumber);
+            this.Controls.Add(this.cmbAlgorithm);
+            this.Controls.Add(this.cmbStartPlayer);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnMult2);
+            this.Controls.Add(this.btnMult3);
+            this.Controls.Add(this.btnMult4);
+            this.Controls.Add(this.lblCurrentNumber);
+            this.Controls.Add(this.lblScores);
+            this.Name = "Form1";
+            this.Text = "Game_Team_37 - AI Game - Minimax vs Alpha-Beta ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameVsComputer
+{
+    public partial class Form1 : Form
+    {
+        enum AlgorithmType { Minimax, AlphaBeta }
+
+        int gameBank = 0;
+        int playerScore = 0;
+        int computerScore = 0;
+        int currentNumber = 0;
+        int maxDepth = 4;
+        bool isPlayerTurn = true;
+        bool gameEnded = false;
+
+        AlgorithmType selectedAlgorithm;
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadComboBoxes();
+        }
+        private void LoadComboBoxes()
+        {
+            cmbAlgorithm.Items.AddRange(new string[] { "Minimax", "Alpha-Beta" });
+            cmbStartPlayer.Items.AddRange(new string[] { "Player", "Computer" });
+
+            cmbAlgorithm.SelectedIndex = 0;      // varsayılan olarak Minimax seçili gelsin
+            cmbStartPlayer.SelectedIndex = 0;    // varsayılan olarak Player seçili gelsin
+        }
+
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtStartNumber.Text, out int num) && num >= 8 && num <= 18)
+            {
+                currentNumber = num;
+                gameBank = 0;
+                playerScore = 0;
+                computerScore = 0;
+                gameEnded = false;
+
+                isPlayerTurn = cmbStartPlayer.SelectedItem.ToString() == "Player";
+                selectedAlgorithm = cmbAlgorithm.SelectedIndex == 0 ? AlgorithmType.Minimax : AlgorithmType.AlphaBeta;
+
+                EnableGameControls(isPlayerTurn);
+                UpdateUI();
+
+                if (!isPlayerTurn)
+                {
+                    await Task.Delay(300);
+                    await ComputerTurn(); // async hale getirildi
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a number between 8 and 18.");
+            }
+        }
+
+        private async void MultiplyAndPlay(int multiplier)
+        {
+            if (gameEnded || !isPlayerTurn) return;
+
+            currentNumber *= multiplier;
+
+            if (currentNumber % 2 == 0) playerScore--;
+            else playerScore++;
+
+            if (currentNumber % 10 == 0 || currentNumber % 10 == 5) gameBank++;
+
+            CheckGameEnd();
+            UpdateUI();
+
+            if (!gameEnded)
+            {
+                isPlayerTurn = false;
+                EnableGameControls(false);
+                await Task.Delay(300);
+                await ComputerTurn();
+            }
+        }
+
+        private async Task ComputerTurn()
+        {
+            await Task.Delay(300);
+
+            if (gameEnded) return;
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            int bestMultiplier = 2;
+            int bestValue = int.MinValue;
+
+            foreach (int mult in new int[] { 2, 3, 4 })
+            {
+                GameState next = new GameState(currentNumber * mult, playerScore, computerScore, gameBank);
+                next.ApplyRules(mult, false);
+
+                int value = selectedAlgorithm == AlgorithmType.Minimax
+                    ? Minimax(next, maxDepth - 1, false)
+                    : AlphaBeta(next, maxDepth - 1, int.MinValue, int.MaxValue, false);
+
+                if (value > bestValue)
+                {
+                    bestValue = value;
+                    bestMultiplier = mult;
+                }
+            }
+            stopwatch.Stop();
+
+            currentNumber *= bestMultiplier;
+            if (currentNumber % 2 == 0) computerScore--;
+            else computerScore++;
+
+            if (currentNumber % 10 == 0 || currentNumber % 10 == 5) gameBank++;
+
+            CheckGameEnd();
+            UpdateUI();
+
+            if (!gameEnded)
+            {
+                isPlayerTurn = true;
+                EnableGameControls(true);
+            }
+        }
+
+        private void CheckGameEnd()
+        {
+            if (currentNumber >= 1200)
+            {
+                gameEnded = true;
+                if (isPlayerTurn) playerScore += gameBank;
+                else computerScore += gameBank;
+
+                EnableGameControls(false);
+
+                string result = playerScore > computerScore ? "🎉 Player wins!" :
+                                computerScore > playerScore ? "🤖 Computer wins!" : "🤝 It's a draw!";
+                MessageBox.Show($"Game Over!\nPlayer: {playerScore}\nComputer: {computerScore}\n{result}");
+            }
+        }
+
+        private void EnableGameControls(bool enable)
+        {
+            btnMult2.Enabled = btnMult3.Enabled = btnMult4.Enabled = enable && isPlayerTurn;
+        }
+
+        private void UpdateUI()
+        {
+            lblCurrentNumber.Text = $"Current Number: {currentNumber}";
+            lblScores.Text = $"Player: {playerScore} | Computer: {computerScore} | Bank: {gameBank}";
+            EnableGameControls(!gameEnded);
+        }
+
+        private int Evaluate(GameState state)
+        {
+            return (state.ComputerScore - state.PlayerScore) + state.Bank;
+        }
+
+        private int Minimax(GameState state, int depth, bool maximizing)
+        {
+            if (depth == 0 || state.CurrentNumber >= 1200)
+                return Evaluate(state);
+
+            int best = maximizing ? int.MinValue : int.MaxValue;
+            foreach (int mult in new int[] { 2, 3, 4 })
+            {
+                GameState child = state.Clone();
+                child.CurrentNumber *= mult;
+                child.ApplyRules(mult, maximizing);
+                int val = Minimax(child, depth - 1, !maximizing);
+                best = maximizing ? Math.Max(best, val) : Math.Min(best, val);
+            }
+            return best;
+        }
+
+        private int AlphaBeta(GameState state, int depth, int alpha, int beta, bool maximizing)
+        {
+            if (depth == 0 || state.CurrentNumber >= 1200)
+                return Evaluate(state);
+
+            foreach (int mult in new int[] { 2, 3, 4 })
+            {
+                GameState child = state.Clone();
+                child.CurrentNumber *= mult;
+                child.ApplyRules(mult, maximizing);
+                int val = AlphaBeta(child, depth - 1, alpha, beta, !maximizing);
+
+                if (maximizing)
+                {
+                    alpha = Math.Max(alpha, val);
+                    if (alpha >= beta) break;
+                }
+                else
+                {
+                    beta = Math.Min(beta, val);
+                    if (beta <= alpha) break;
+                }
+            }
+            return maximizing ? alpha : beta;
+        }
+
+        private void btnMult2_Click(object sender, EventArgs e) => MultiplyAndPlay(2);
+        private void btnMult3_Click(object sender, EventArgs e) => MultiplyAndPlay(3);
+        private void btnMult4_Click(object sender, EventArgs e) => MultiplyAndPlay(4);
+
+    }
+
+    public class GameState
+    {
+        public int CurrentNumber;
+        public int PlayerScore;
+        public int ComputerScore;
+        public int Bank;
+
+        public GameState Parent;
+        public List<GameState> Children = new List<GameState>();
+        public int LastMove;
+        public int Depth;
+
+        public GameState(int num, int pScore, int cScore, int bank, GameState parent = null, int lastMove = 0, int depth = 0)
+        {
+            CurrentNumber = num;
+            PlayerScore = pScore;
+            ComputerScore = cScore;
+            Bank = bank;
+            Parent = parent;
+            LastMove = lastMove;
+            Depth = depth;
+        }
+
+        public void ApplyRules(int multiplier, bool isPlayer)
+        {
+            if (CurrentNumber % 2 == 0)
+            {
+                if (isPlayer) PlayerScore--;
+                else ComputerScore--;
+            }
+            else
+            {
+                if (isPlayer) PlayerScore++;
+                else ComputerScore++;
+            }
+
+            if (CurrentNumber % 10 == 0 || CurrentNumber % 10 == 5)
+                Bank++;
+        }
+
+        public GameState Clone()
+        {
+            return new GameState(CurrentNumber, PlayerScore, ComputerScore, Bank, Parent, LastMove, Depth);
+        }
+
+        public void GenerateChildren(bool isPlayer)
+        {
+            foreach (int multiplier in new[] { 2, 3, 4 })
+            {
+                GameState child = Clone();
+                child.CurrentNumber *= multiplier;
+                child.ApplyRules(multiplier, isPlayer);
+                child.Parent = this;
+                child.LastMove = multiplier;
+                child.Depth = this.Depth + 1;
+                this.Children.Add(child);
+            }
+        }
+    }
+    }
